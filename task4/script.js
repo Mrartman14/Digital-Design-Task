d244094009deee8bf9597832f864001f6a5c3086
Александр <mrartman14@gmail.com>
2019-10-15 21:14:49 +0400

    task4 15.10.19

---
+++ task4/script.js
@@ -0,0 +1,134 @@
+//---------------------------------1---------------------------------//
+for (let i = 0; i < 7; i++) {
+	let str = "";
+	for (let a = 0; a <= i; a++) {
+		str += "#";
+	}
+	console.log(str);
+}
+//---------------------------------2---------------------------------//
+let colNum = 8;
+for (let i = 0; i < colNum; i++) {
+	let str = "";
+	if (i % 2 == 0) {
+		for (let a = 0; a < colNum; a++) {
+			if (a % 2 == 0) {
+				str += "#";
+			} else {
+				str += " ";
+			}
+		}
+	} else {
+		for (let a = 0; a < colNum; a++) {
+			if (a % 2 == 0) {
+				str += " ";
+			} else {
+				str += "#";
+			}
+		}
+	}
+	console.log(str);
+}
+
+//---------------------------------3---------------------------------//
+function min(a, b) {
+	if (a < b) {
+		return a;
+	} else return b;
+}
+console.log(min(0, 10));
+console.log(min(0, -10));
+
+//---------------------------------4---------------------------------//
+function range(a, b, step = 1) {
+	let rangeArray = [];
+	if (b - a < 0) {
+		for (let i = 0; i * Math.abs(step) < -(b - a) + 1; i++) {
+			rangeArray[i] = a - i * Math.abs(step);
+		}
+	} else {
+		for (let i = 0; i * Math.abs(step) < (b - a) + 1; i++) {
+			rangeArray[i] = a + i * Math.abs(step);
+		}
+	}
+	return rangeArray;
+}
+console.log(range(5, 2, -1));
+
+function sum(array) {
+	let amount = 0;
+	for (let i = 0; i < array.length; i++) {
+		amount += array[i];
+	}
+	return amount;
+}
+console.log(sum(range(1, 10)));
+
+
+
+//---------------------------------5---------------------------------//
+function reverseArray(a) {
+	let newArr = [];
+	for (let i = 0; i < a.length; i++) {
+		newArr[i] = a[a.length - i - 1];
+	}
+	return newArr;
+}
+console.log(reverseArray(["A", "B", "C"]));
+
+function reverseArrayInPlace(a) {
+	let newArr = [];
+	for (let i = 0; i < a.length; i++) {
+		newArr[i] = a[a.length - i - 1];
+	}
+	for (let i = 0; i < a.length; i++) {
+		a[i] = newArr[i];
+	}
+	return a;
+}
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
+
+//---------------------------------6---------------------------------//
+let arrays = [[1, 2, 3], [4, 5], [6]];
+let result = arrays.reduce((sum, current) => {
+	return sum.concat(current);
+});
+console.log(result);
+
+//---------------------------------7---------------------------------//
+function deepEqual(a, b) {
+	if (typeof a === "object" && typeof b === "object" && a !== null && b !== null) {
+		let aValues = Object.values(a);
+		let bValues = Object.values(b);
+		if (aValues.toString() === bValues.toString()) {
+			return true;
+		} else return false;
+	} else {
+		if (a === b) {
+			return true;
+		} else return false;
+	}
+}
+
+let obj = {
+	here: {
+		is: "an"
+	},
+	object: 2
+}
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
+
+//---------------------------------8---------------------------------//
+let month = function (a) {
+	let months = ["January", "February", "March", "April", "May", "June", "July", "August", "Sentember", "October", "November", "December"];
+	return {
+		number: (num) => { return months[num] },
+		name: (name) => { return months.indexOf(name) }
+	}
+}();
+console.log(month.number(2));
+console.log(month.name("November"));
